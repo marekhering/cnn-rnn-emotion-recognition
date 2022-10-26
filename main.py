52587a43754b471cffae8ba01f5a775aef8464c0
Marek Hering <marek.hering99@gmail.com>
2022-10-26 21:23:46 +0200

    change default video source to camera

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src import App
 
 if __name__ == '__main__':
     app = App()
-    app.videos_inference()  # For getting inference results from videos files
-    # app.video_inference(0, vis=True, save=False)  # For webcam emotion recognition
+    # app.videos_inference()  # For getting inference results from videos files
+    app.video_inference(0, vis=True, save=False)  # For webcam emotion recognition
     # app.video_inference('data/input/videos/vid1.mp4', vis=True)  # For emotion recognition from file
     # app.image_inference('data/input/img/00001.jpg')
